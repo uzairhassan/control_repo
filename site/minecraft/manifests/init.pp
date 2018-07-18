66802ae984eef82f4cd77c25790e17b33af00a9a
Uzair Hassan <uzairhassan96@gmail.com>
2018-07-18 12:21:02 +0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,25 +5,25 @@ class minecraft {
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
-    before => Service[‘minecraft’],
+    before => Service['minecraft'],
   }
 
 
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt’],File[‘/etc/systemd/system/minecraft.service’]],
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
 
